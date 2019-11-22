f0e9c2b99391f804fcc234cbb3543226152be88a
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-22 07:40:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,4 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN")
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
